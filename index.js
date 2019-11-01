70b5df3925e2c609551456efac60b8b0a8cd34f6
gabrielvv-tfm <gvautrin.external@lafourchette.com>
2019-11-01 16:39:20 +0100

    feat: play with deployments

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const Octokit = require("@octokit/rest");
 
 const octokit = Octokit({
-    token: process.env.GITHUB_TOKEN
+    auth: process.env.GITHUB_TOKEN
 });
 
-octokit.repos.createDeployment({
+// octokit.projects.listForOrg({
+//     org: 'gabrielvv-tfm'
+//   }).then(console.log)
+// .catch(console.error)
+
+
+// octokit.repos.createDeployment({
+//     owner: 'gabrielvv-tfm',
+//     repo: 'play-with-github',
+//     ref: 'master',
+//     description: 'test'
+// })
+// .then(console.log)
+// .catch(console.error)
+
+octokit.repos.createDeploymentStatus({
     owner: 'gabrielvv-tfm',
     repo: 'play-with-github',
-    ref: 'master',
-    description: 'test'
-}).then(console.log)
+    deployment_id: 179287269,
+    description: 'lol',
+    environment_url: '',
+    log_url: 'https://github.com/gabrielvv-tfm/play-with-github',
+    state: 'success',
+    mediaType: {
+        previews: ["flash"]
+      }
+  })
