107feec172bfe54e725a2c5d92e8921842ee8d51
gabrielvv-tfm <gvautrin.external@lafourchette.com>
2019-11-01 17:16:58 +0100

    init probot app

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-const Octokit = require("@octokit/rest");
+// Checks API example
+// See: https://developer.github.com/v3/checks/ to learn more
 
-const octokit = Octokit({
-    auth: process.env.GITHUB_TOKEN
-});
+/**
+ * This is the main entrypoint to your Probot app
+ * @param {import('probot').Application} app
+ */
+module.exports = app => {
+  app.on(['check_suite.requested', 'check_run.rerequested'], check)
 
-// octokit.projects.listForOrg({
-//     org: 'gabrielvv-tfm'
-//   }).then(console.log)
-// .catch(console.error)
+  async function check (context) {
+    const startTime = new Date()
 
-
-// octokit.repos.createDeployment({
-//     owner: 'gabrielvv-tfm',
-//     repo: 'play-with-github',
-//     ref: 'master',
-//     description: 'test'
-// })
-// .then(console.log)
-// .catch(console.error)
-
-octokit.repos.createDeploymentStatus({
-    owner: 'gabrielvv-tfm',
-    repo: 'play-with-github',
-    deployment_id: 179287269,
-    description: 'lol',
-    environment_url: '',
-    log_url: 'https://github.com/gabrielvv-tfm/play-with-github',
-    state: 'success',
-    mediaType: {
-        previews: ["flash"]
+    // Do stuff
+    const { head_branch: headBranch, head_sha: headSha } = context.payload.check_suite
+    // Probot API note: context.repo() => {username: 'hiimbex', repo: 'testing-things'}
+    return context.github.checks.create(context.repo({
+      name: 'My app!',
+      head_branch: headBranch,
+      head_sha: headSha,
+      status: 'completed',
+      started_at: startTime,
+      conclusion: 'success',
+      completed_at: new Date(),
+      output: {
+        title: 'Probot check!',
+        summary: 'The check has passed!'
       }
-  })
+    }))
+  }
+
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+}
